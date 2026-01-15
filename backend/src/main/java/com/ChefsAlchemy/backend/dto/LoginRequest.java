2ef0f0de6a28cfc2fa3b5bb651d2c5e5cb9d74cc
skyp3crack <khalifamin99@gmail.com>
2026-01-16 03:36:16 +0800

    feat: add DTOs for login requests and JWT responses.

---
+++ backend/src/main/java/com/ChefsAlchemy/backend/dto/LoginRequest.java
@@ -0,0 +1,42 @@
+package com.ChefsAlchemy.backend.dto;
+
+//These DTOs will be used for the login request and response.
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class LoginRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    // Constructors
+    public LoginRequest() {
+    }
+
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
