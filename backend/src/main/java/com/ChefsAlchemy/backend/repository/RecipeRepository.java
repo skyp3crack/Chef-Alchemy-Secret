9b187900c5934928499898fece1253eb2848e0e4
skyp3crack <khalifamin99@gmail.com>
2026-01-16 09:22:43 +0800

    Create RecipeRepository act as an interface for recipe entity

---
+++ backend/src/main/java/com/ChefsAlchemy/backend/repository/RecipeRepository.java
@@ -0,0 +1,11 @@
+import org.springframework.stereotype.Repository;
+
+import com.ChefsAlchemy.backend.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+//recipe repository act as an interface for recipe entity
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    List<Recipe> findByAuthorId(Long userId); // find recipes by specific user
+}
